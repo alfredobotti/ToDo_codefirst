2771823b12e224211707d07543e4e5d52024ebdb
alfredo <alfredobotti1@gmail.com>
2020-08-04 20:11:25 +0200

    inserire l'oggetto 'alfredo' di tipo E_Risorsa nel Dbset risultava impossibile causa errore aderente al campo 'ID_Risorsa'; ho risolto modificando lo script con l'aggiunta di un IDENTITY CONSTRAINT sulla colonna ID_Risorsa. Ho provveduto in seguito a creare 2 oggetti di tipo 'E_Task' ed ho provato ad aggiungerli ad Dbset E_Task; a questo punto si è presentata l'eccezione System.Data.Entity.Infrastructure.DbUpdateException: 'An error occurred while saving entities that do not expose foreign key properties for their relationships.  The EntityEntries property will return null because a single entity cannot be identified as the source of the exception.  Handling of exceptions while saving can be made easier by exposing foreign key properties in your entity types.

---
+++ codefirst1/codefirst1/Migrations/202007301317266_firstmigration.cs
@@ -0,0 +1,98 @@
+﻿namespace codefirst1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstmigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.E_ProjectItem",
+                c => new
+                    {
+                        ID_ProjectItem = c.Int(nullable: false, identity: true),
+                        Titolo = c.String(),
+                        Descrizione = c.String(),
+                        ID_CategoriaProjectItem = c.Int(nullable: false),
+                        DataInserimento = c.DateTime(nullable: false),
+                        DataUltimoAggiornamento = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID_ProjectItem)
+                .ForeignKey("dbo.T_CategoriaProjectItem", t => t.ID_CategoriaProjectItem, cascadeDelete: true)
+                .Index(t => t.ID_CategoriaProjectItem);
+            
+            CreateTable(
+                "dbo.T_CategoriaProjectItem",
+                c => new
+                    {
+                        ID_CategoriaProjectItem = c.Int(nullable: false, identity: true),
+                        Nome = c.Binary(),
+                        Descrizione = c.String(),
+                        DataInserimento = c.DateTime(nullable: false),
+                        DataUltimaModifica = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID_CategoriaProjectItem);
+            
+            CreateTable(
+                "dbo.E_Risorsa",
+                c => new
+                    {
+                        ID_Risorsa = c.Int(nullable: false, identity: true),
+                        MatricolaRisorsa = c.String(),
+                        Cognome = c.String(),
+                        Nome = c.String(),
+                        Mail = c.String(),
+                        Telefono = c.String(),
+                        Cellulare = c.String(),
+                    })
+                .PrimaryKey(t => t.ID_Risorsa);
+            
+            CreateTable(
+                "dbo.E_Task",
+                c => new
+                    {
+                        ID_Task = c.Int(nullable: false, identity: true),
+                        Titolo = c.String(),
+                        Descrizione = c.String(),
+                        ID_ParentTask = c.Int(),
+                        DataInserimento = c.DateTime(nullable: false),
+                        DataUltimoAggiornamento = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID_Task)
+                .ForeignKey("dbo.E_Task", t => t.ID_ParentTask)
+                .Index(t => t.ID_ParentTask);
+            
+            CreateTable(
+                "dbo.E_TaskE_Risorsa",
+                c => new
+                    {
+                        E_Task_ID_Task = c.Int(nullable: false),
+                        E_Risorsa_ID_Risorsa = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.E_Task_ID_Task, t.E_Risorsa_ID_Risorsa })
+                .ForeignKey("dbo.E_Task", t => t.E_Task_ID_Task, cascadeDelete: true)
+                .ForeignKey("dbo.E_Risorsa", t => t.E_Risorsa_ID_Risorsa, cascadeDelete: true)
+                .Index(t => t.E_Task_ID_Task)
+                .Index(t => t.E_Risorsa_ID_Risorsa);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.E_Task", "ID_ParentTask", "dbo.E_Task");
+            DropForeignKey("dbo.E_TaskE_Risorsa", "E_Risorsa_ID_Risorsa", "dbo.E_Risorsa");
+            DropForeignKey("dbo.E_TaskE_Risorsa", "E_Task_ID_Task", "dbo.E_Task");
+            DropForeignKey("dbo.E_ProjectItem", "ID_CategoriaProjectItem", "dbo.T_CategoriaProjectItem");
+            DropIndex("dbo.E_TaskE_Risorsa", new[] { "E_Risorsa_ID_Risorsa" });
+            DropIndex("dbo.E_TaskE_Risorsa", new[] { "E_Task_ID_Task" });
+            DropIndex("dbo.E_Task", new[] { "ID_ParentTask" });
+            DropIndex("dbo.E_ProjectItem", new[] { "ID_CategoriaProjectItem" });
+            DropTable("dbo.E_TaskE_Risorsa");
+            DropTable("dbo.E_Task");
+            DropTable("dbo.E_Risorsa");
+            DropTable("dbo.T_CategoriaProjectItem");
+            DropTable("dbo.E_ProjectItem");
+        }
+    }
+}
---
+++ codefirst1/codefirst1/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDocodefirst
+{
+    public partial class E_Risorsa
+    {
+
+        public E_Risorsa()
+        {
+            this.E_Task = new HashSet<E_Task>();
+        }
+
+        [Key]
+        public int ID_Risorsa { get; set; }
+        public string MatricolaRisorsa { get; set; }
+        public string Cognome { get; set; }
+        public string Nome { get; set; }
+        public string Mail { get; set; }
+        public string Telefono { get; set; }
+        public string Cellulare { get; set; }
+
+
+        public virtual ICollection<E_Task> E_Task { get; set; }
+    }
+
+    public partial class ToDocodefirstEntities : DbContext
+    {
+        public ToDocodefirstEntities()
+            : base("name=ToDocodefirstEntities")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<E_Task>()
+                 .HasMany(t => t.Listasottotask)
+                 .WithOptional(t => t.Parenttask)
+                 .HasForeignKey(t => t.ID_ParentTask);
+        }
+
+        public virtual DbSet<E_ProjectItem> E_ProjectItem { get; set; }
+        public virtual DbSet<E_Risorsa> E_Risorsa { get; set; }
+        public virtual DbSet<E_Task> E_Task { get; set; }
+        public virtual DbSet<T_CategoriaProjectItem> T_CategoriaProjectItem { get; set; }
+
+
+    }
+
+    public partial class E_ProjectItem
+    {
+        [Key]
+        public int ID_ProjectItem { get; set; }
+        public string Titolo { get; set; }
+        public string Descrizione { get; set; }
+        public int ID_CategoriaProjectItem { get; set; }
+        public System.DateTime DataInserimento { get; set; }
+        public System.DateTime DataUltimoAggiornamento { get; set; }
+
+        public virtual T_CategoriaProjectItem T_CategoriaProjectItem { get; set; }
+    }
+
+    public partial class E_Task
+    {
+
+       
+
+        [Key]
+        public int ID_Task { get; set; }
+        public string Titolo { get; set; }
+        public string Descrizione { get; set; }
+        public Nullable<int> ID_ParentTask { get; set; }
+        public System.DateTime DataInserimento { get; set; }
+        public System.DateTime DataUltimoAggiornamento { get; set; }
+
+
+        public virtual ICollection<E_Task> Listasottotask { get; set; }
+        public virtual E_Task Parenttask { get; set; }
+
+        public virtual ICollection<E_Risorsa> Listarisorseassegnate { get; set; }
+    }
+
+    public partial class T_CategoriaProjectItem
+    {
+
+        public T_CategoriaProjectItem()
+        {
+            this.E_ProjectItem = new HashSet<E_ProjectItem>();
+        }
+        [Key]
+        public int ID_CategoriaProjectItem { get; set; }
+        public byte[] Nome { get; set; }
+        public string Descrizione { get; set; }
+        public System.DateTime DataInserimento { get; set; }
+        public System.DateTime DataUltimaModifica { get; set; }
+
+        public virtual ICollection<E_ProjectItem> E_ProjectItem { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDocodefirstEntities context = new ToDocodefirstEntities();
+             E_Risorsa alfredo = new E_Risorsa() {
+                                                 ID_Risorsa=1,
+                                                 MatricolaRisorsa="12345",
+                                                 Cognome="Botti",
+                                                 Nome="Alfredo",
+                                                 Mail="alfredobotti1@gmail.com",
+                                                 Cellulare="3661958952"};
+             context.E_Risorsa.Add(alfredo);
+            E_Task Task1 = new E_Task()
+            {
+
+                                                ID_Task = 1,
+                                                Titolo = "revisione progetto",
+                                                Descrizione = "revisione intero progetto",
+                                                ID_ParentTask = null,
+                                                DataInserimento = new DateTime(2008, 5, 1),
+                                                DataUltimoAggiornamento = new DateTime(2008, 5, 1),
+                                                Listasottotask = new List<E_Task>(),
+                                                Parenttask = new E_Task(),
+                                                Listarisorseassegnate = new List<E_Risorsa>()
+
+            };
+            E_Task Task2 = new E_Task()
+            {
+
+                                                ID_Task = 2,
+                                                Titolo = "revisione codice",
+                                                Descrizione = "revisione codice sorgente",
+                                                ID_ParentTask = 1,
+                                                DataInserimento = new DateTime(2008, 5, 1),
+                                                DataUltimoAggiornamento = new DateTime(2008, 5, 1),
+                                                Parenttask = new E_Task(),
+                                                Listasottotask = new List<E_Task>(),
+                                                Listarisorseassegnate= new List<E_Risorsa>()
+
+            };
+            
+            
+            
+            context.E_Task.Add(Task1);
+            //Task1.Listasottotask.Add(Task2);
+            //Task1.Listarisorseassegnate.Add(alfredo);
+           
+
+            //context.E_Task.Add(Task2);
+           //Task2.Parenttask=Task1;
+           //Task2.Listarisorseassegnate.Add(alfredo);
+
+            
+            context.SaveChanges();
+
+
+            var result = context.E_Risorsa.Select(ris => new
+            {
+                Nome = ris.Nome,
+                Cognome = ris.Cognome
+            });
+            foreach (var v in result)
+            {
+                Console.WriteLine(v.Nome + " - " + v.Cognome);
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+}
